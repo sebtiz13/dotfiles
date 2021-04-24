e0b695600d1b98faaba7599c8d97d578b0878638
Sébastien <sebtiz13@gmail.com>
2021-04-24 17:09:39 +0200

    fix(xmodmap): fix freeze caused by Xmodmap
    Export full keymap table cause freeze with KDE plasma

--- install-xmodmap.sh
+++ install-xmodmap.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
-if [ -x "$(command -v xmodmap)" ]; then
+if [[ -x "$(command -v xmodmap)" && ! -f $HOME/.Xmodmap ]]; then
   # Apply change on xmodmap
   for expression in "$(grep -o '^[^#]*' templates/xmodmap)"; do
+    echo "$expression" >> $HOME/.Xmodmap
     xmodmap -e "$expression"
   done
-  xmodmap -pke > $HOME/.Xmodmap
+  # Export full keymap table cause freeze with KDE plasma
+  # xmodmap -pke > $HOME/.Xmodmap
+elif [ -f $HOME/.Xmodmap ]; then
+  echo "$HOME/.Xmodmap already exist"
 else
   echo "xmodmap it's not available"
 fi
