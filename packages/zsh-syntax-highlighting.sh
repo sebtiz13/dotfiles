99f5ceb7c9adec1f2120c62da27a671e0df5d7f2
Dorian Rodriguez <orblazer.minecraft@gmail.com>
2021-05-22 00:44:22 +0200

    fix(packages): use right name in question

--- packages/zsh-syntax-highlighting.sh
+++ packages/zsh-syntax-highlighting.sh
@@ -2,7 +2,7 @@
 
 function zsh-syntax-highlighting::install() {
   # Ask if want install skip if respond no
-  question "Do you want install zsh-autosuggestions ?" y || return
+  question "Do you want install zsh-syntax-highlighting ?" y || return
 
   [ ! -n "$ZSH_CUSTOM" ] && { echo "ZSH_CUSTOM is not set"; exit 1; }
 
